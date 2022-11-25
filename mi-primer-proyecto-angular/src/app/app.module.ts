dbd6326812afdc9fa18ed5f32e230a25fc6d69fb
arielschinocca <96122546+arielschinocca@users.noreply.github.com>
2022-11-24 21:16:28 -0300

    se modifico todo

--- mi-primer-proyecto-angular/src/app/app.module.ts
+++ mi-primer-proyecto-angular/src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { MiPrimerComponenteComponent } from './clase2/mi-primer-componente/mi-primer-componente.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MiPrimerComponenteComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
